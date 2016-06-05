dfa1b604df42671dd68f14e1adb8ff8c01c85608
vagrant <vagrant@homestead>
2016-06-05 21:51:38 +0000

    video 9- Forms

--- resources/views/cards/index.blade.php
+++ resources/views/cards/index.blade.php
@@ -8,8 +8,9 @@
 
     @foreach($cards as $card)
 
-        <a href="{{ $card->path() }}">{{ $card->title }}</a>
-
+        <p>
+            <a href="{{ $card->path() }}">{{ $card->title }}</a>
+        </p>
         <!--There are a few ways you can do this, probably the most common and best is calling: "/cards/{ {$cards->id} }" ,
             (not the { { blade syntax } } is modified for commenting purposes)
          but this method is in place to demonstrate how to hyperlink through function in the model   -->
